ee2b3081bd58463c5e68be7a1c438de2318a6b97
sadiyebuyukcukurlu <58874305+Sadiyebuyukcukurlu@users.noreply.github.com>
2021-09-01 08:08:25 -0700

    Update execute_s.py

--- execute_s.py
+++ execute_s.py
@@ -23,10 +23,10 @@ class inputSC(StreamCallback): #fonksiyonlarımı buranın içinde kullanacağim
 
       time_regex=re.search("(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9])",logs) #hepsine uyuyor
 
-      srcip_forti=re.search("(?<=srcip=)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs) 
-      srcip_check=re.search("(?<=src=\W+)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs) 
-      srcip_sophos=re.search("(?<=src_ip=)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs) 
-      srcip_palo=re.search("(?<=src=)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs)
+      srcip_forti=re.search("(?<=srcip=)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs) 
+      srcip_check=re.search("(?<=src=\W+)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs) 
+      srcip_sophos=re.search("(?<=src_ip=)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs) 
+      srcip_palo=re.search("(?<=src=)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs)
 
       type_forti=re.search("(?<=type=\W+)\w+",logs)
       type_check=re.search("")
@@ -53,10 +53,60 @@ class inputSC(StreamCallback): #fonksiyonlarımı buranın içinde kullanacağim
       src_interface_sophos=re.search("(?<=in_interface\W+)\w+\W+\w+")
       src_interface_palo=re.search("")
 
-      dstip_forti=re.search("(?<=dstip\W+)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs)
+      dstip_forti=re.search("(?<=dstip\W+)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs)
       dstip_check=re.search
-      dstip_sophos=re.search("(?<=dst_ip\W+)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs)
-      dstip_palo=re.search("(?<=dst\W+)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",logs)
+      dstip_sophos=re.search("(?<=dst_ip\W+)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs)
+      dstip_palo=re.search("(?<=dst\W+)(?:[0-9]{1,3}\.){3}[0-9]{1,3}",logs)
+
+      dstport_forti=re.search("(?<=dstport=)\w+")
+      dstport _check=re.search("")
+      dstport_sophos=re.search("(?<=dst_port=)\w+",logs)
+      dstport_palo=re.search("(?<=dstPort=)\w+",logs)
+
+      fpolicy_forti=re.search("(?<=action=\W+)\w+",logs)
+      fpolicy_check=re.search("(?<=Action=\W+)\w+",logs)
+      fpolicy_sophos=re.search("")
+      fpolicy_palo=re.search("(?<=action=)\w+",logs)
+
+      NatSrcIp_forti=re.search("(?<=transip=)(?:[0-9]{1,3}\.){3}[0-9]{1,3}"logs)  
+      NatSrcIp_check=re.search("")
+      NatSrcIp_sophos=re.search("")
+      NatSrcIp_palo=re.search("(?<=srcPostNATPort=)\w+",logs)
+
+      sentbyte_forti=re.search("(?<=sentbyte=)\w+",logs)
+      sentbyte_check=re.search("")
+      sentbyte_sophos=re.search("(?<=sent_bytes=)\w+",logs)
+      sentbyte_palo=re.search("")
+
+      pcktrcivd_forti=re.search("(?<=rcvdpkt=)\w+",logs)
+      pcktrcivd_check=re.search("")
+      pcktrcivd_sophos=re.search("")
+      pcktrcivd_palo=re.search("")
+
+      os_forti=re.search("(?<=osname\W+)\w+",logs)
+      os_check=re.search("")
+      os_sophos=re.search("")
+      os_palo=re.search("")
+
+      app_forti=re.search("(?<=app\W+)\S+(\w+)",logs)
+      app_check=re.search("(?<=application\W+)\w+.\w+",logs)
+      app_sophos=re.search("")
+      app_palo=re.search("")
+
+      sentpkt_forti=re.search("(?<=sentpkt\W+)\w+",logs)
+      sentpkt_check=re.search("")
+      sentpkt_sophos=re.search("(?<=sent_pkts\W+)\w+",logs)
+      sentpkt_palo=re.search("")
+
+      duration_forti=re.search("(?<=duration\W+)\w+",logs) 
+      duration_check=re.search("")
+      duration_sophos=re.search("(?<=duration\W+)\w+")
+      duration_palo=re.search("")
+
+      proto_forti=re.search("(?<=proto\W+)\w+",logs)
+      proto_check=re.search("")
+      proto_sophos=re.search("(?<=protocol\W+)\w+",logs)
+      proto_palo=re.search("(?<=proto\W+)\w+",logs)
 
       _forti=re.search
       _check=re.search
@@ -67,9 +117,3 @@ class inputSC(StreamCallback): #fonksiyonlarımı buranın içinde kullanacağim
       _check=re.search
       _sophos=re.search
       _palo=re.search
-
-
-
-
-      
-      
