6cb15592eac30a7c854dcb723c41b0da33b218f8
sadiyebuyukcukurlu <58874305+Sadiyebuyukcukurlu@users.noreply.github.com>
2021-09-09 13:58:24 -0700

    Update execute_s.py

--- execute_s.py
+++ execute_s.py
@@ -6,17 +6,17 @@ import re
 class TransformCallback(StreamCallback):
   def __init__(self):
     pass
-    def process(self, input, output):
-      try:
-        logs=IOUtils.toString(input, StandardCharsets.UTF_8):
-        logid_forti=re.search("(?<=logid=\W+)\w+",logs)
-        if logid_forti:
-          logid=("LogID="+srt(logid_forti))
-          time_forti=re.search("(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9])",logs)
-          time=("Time="+str(time_forti))
+  def process(self, input, output):
+    try:
+      logs=IOUtils.toString(input, StandardCharsets.UTF_8)
+      logid_forti=re.search("(?<=logid=\W+)\w+",logs)
+      if logid_forti:
+        logid=("LogID="+srt(logid_forti))
+        time_forti=re.search("(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9])",logs)
+        time=("Time="+str(time_forti))
          
-        except:
-          pass
+    except:
+      pass
 
 flowFile = session.get()
 if flowFile != None:
