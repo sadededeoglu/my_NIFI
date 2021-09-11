975ec12b2eec43a8e02b024929d53cade9450e1d
sadiyebuyukcukurlu <58874305+Sadiyebuyukcukurlu@users.noreply.github.com>
2021-09-11 06:03:19 -0700

    Update execute_s.py

--- execute_s.py
+++ execute_s.py
@@ -3,20 +3,25 @@ from org.apache.nifi.processor.io import StreamCallback
 from java.nio.charset import StandardCharsets
 from org.apache.commons.io import IOUtils
 import re
-class TransformCallback(StreamCallback):
+
+class PyInputStreamCallback(StreamCallback):
+
   def __init__(self):
     pass
+
   def process(self, inputStream, outputStream):
+
+    logs=IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+
     try:
-      logs=IOUtils.toString(inputStream, StandardCharsets.UTF_8)
       logid_forti=re.search("(?<=logid=\W+)\w+",logs)
       if logid_forti:
         logid=("LogID="+srt(logid_forti))
-        outputStream.write(logid)    
+        outputStream.write(IOUtils.toBytes(logid))    
     except:
       pass
 
 flowFile = session.get()
-if flowFile != None:
-  flowFile = session.write(flowFile, TransformCallback())
+if (flowFile != None):
+  flowFile = session.write(flowFile, PyInputStreamCallback())
   session.transfer(flowFile, REL_SUCCESS)
